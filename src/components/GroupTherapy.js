52f25e12b5b4680a5018911b88e03ae35b145cb1
Alphaandsew <alfonso.martinez@shrikefound.com>
2021-03-17 12:09:25 -0700

    added group therapy text

--- src/components/GroupTherapy.js
+++ src/components/GroupTherapy.js
@@ -10,6 +10,25 @@ const GroupTherapy = ({ img }) => {
       cta="CTA"
       position="50% 34%"
       />
+
+
+      <main>
+        {/* <h2 className="subtitle">Martha S. Webb, Licensed Clinical Social Worker</h2> */}
+        <h1 className="title">Life Management Therapy Group</h1>
+
+        <section className="row container">
+          <div className="column">
+            <p>
+              I guide a stress management group based on the three pronged approach to healthcare: the cognitive approach, relaxation skills, and expression of positive into your life and negative out of your life.
+            </p>
+            <p>
+              With the three pronged approach, we will learn how to cope with life's daily activities, manage our hurts and griefs, discuss our plans, and set and work towards our goals.
+            </p>
+          </div>
+        </section>
+      </main>
+
+
     </div>
   )
 }
--- src/components/IndividualCounseling.js
+++ src/components/IndividualCounseling.js
@@ -9,6 +9,18 @@ const IndividualCounseling = ({ img }) => {
       description="Learn to live in the moment instead of dwelling on the past or worrying about the future. "
       cta="CTA"
       />
+
+      <main>
+        <h1 className="title">Individual Counseling</h1>
+
+        <section className="row container">
+          <div className="column">
+          <p>No one is immune to life’s ups and downs. I am here for you when you need someone who understands, someone who believes in you. I focus on your strengths as a person, helping you to reach your full potential, even when you don’t fully believe in yourself.</p>
+          <p>I have developed what I call the “Three Pronged Approach to Self Care”. I will coach you to do self care in three categories, which are cognitive, relaxation and expression. I will help you to improve your world view, to use holistic methods to promote relaxation and guide you to release negative feelings, allowing you to become the best and happiest you.</p>
+          </div>
+        </section>
+      </main>
+
     </div>
   )
 }
