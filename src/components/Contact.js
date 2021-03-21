fb688566d163af0e0494c94842727241a2f3e8fc
Alfonso <61892420+ShrikeFound@users.noreply.github.com>
2021-03-20 18:57:21 -0700

    contact form modifications

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,16 +10,23 @@ const Contact = ({ img }) => {
         cta="CTA"
         position = "30% 45%"
       />
-      <section>
-        <form action="https://fieldgoal.io/f/X3qjIM" method="POST">
+      <section className="container">
+        <form  className="contact-form" action="https://fieldgoal.io/f/X3qjIM" method="POST">
+          <div>
           <h1>Form Title</h1>
-          <label for="email">Your Email</label>
-          <input name="Email" id="email" type="email" />
-          <label for="email">Your Name</label>
-          <input name="Name" id="name" type="text" />
-          <label for="message">Your Message</label>
-          <textarea name="message" rows="6" cols="50">Enter text here...</textarea>
-          <button type="submit">Submit</button>
+          <p>
+              Have you ever run into the problem of creating a site with pictures given by the client, only to find later after they update their photography the original look and feel is not retained? This solution involves creating a transparent PNG overlay which can be used as a mask / frame around regular JPEG or GIF.
+          </p>
+          </div>
+          <div>
+            <label for="email">Your Email</label>
+            <input name="Email" id="email" type="email" />
+            <label for="email">Your Name</label>
+            <input name="Name" id="name" type="text" />
+            <label for="message">Your Message</label>
+            <textarea name="message" rows="6" cols="50">Enter text here...</textarea>
+            <button className="button" type="submit">Submit</button>
+            </div>
         </form>
       </section>
     </div>
